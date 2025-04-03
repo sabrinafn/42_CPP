21fa227139ccb002e2e240362bace6343c126a23
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2025-04-03 17:05:04 -0300

    cpp00 added

---
+++ module_00/megaphone.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+void	to_upper(std::string str)
+{
+	int	i;
+	char	upper;
+
+	for (i = 0; i < str.length(); i++)
+	{
+		upper = (char)std::toupper(str[i]);
+		std::cout << upper;
+
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int i = 1;
+	if (ac > 1)
+	{
+		while (av[i] != NULL)
+		{
+			to_upper(av[i]);
+			i++;
+		}
+	}
+	return 0;
+}
