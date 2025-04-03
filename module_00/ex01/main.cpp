53d5edb909b15bf65cf8c74315e5a7c5d9c537cd
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2025-04-03 20:16:17 -0300

    in progress

--- module_00/ex01/main.cpp
+++ module_00/ex01/main.cpp
@@ -7,7 +7,7 @@
 int	main(void)
 {
 	std::cout << "phonebook?" << std::endl;
-	std::string	name;
+	
 	std::cout << "First name:" << std::endl;
 	std::cin >> name;
 	std::cout << "Your name is " << name << std::endl;
--- module_00/ex01/phonebook.hpp
+++ module_00/ex01/phonebook.hpp
@@ -16,12 +16,13 @@ class PhoneBook
 
 class Contact
 {
-	int			index;
-	std::string	first_name;
-	std::string	last_name;
-	std::string nickname;
-	int			phone_number;
-	std::string	darkest_secret;
+	public:
+		int			index;
+		std::string	first_name;
+		std::string	last_name;
+		std::string nickname;
+		int			phone_number;
+		std::string	darkest_secret;
 };
 
 #endif
