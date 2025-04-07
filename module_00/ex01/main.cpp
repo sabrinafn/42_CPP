7a80bd990a6033985a435dfffaa9f8a31a16d28d
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2025-04-07 18:10:50 -0300

    already getting user input

--- module_00/ex01/main.cpp
+++ module_00/ex01/main.cpp
@@ -17,15 +17,31 @@ int	main(void)
 	// b.contact(a);
 	// b = a;
 	Phonebook	phonebook;
-	Contact		NewContact;
-	std::string name;
+	Contact		new_contact;
+	std::string first_name;
+	std::string last_name;
+	std::string nickname;
+	std::string phone_number;
+	std::string darkest_secret;
 	int			i = 0;
 
-	while (i < 8)
-	{
+	//while (i < 8)
+	//{
 		std::cout << "First name:" << std::endl;
-		std::cin >> name;
-		NewContact.setFirstName(name);
-		i++;
-	}
+		std::cin >> first_name;
+		new_contact.setFirstName(first_name);
+		std::cout << "Last name:" << std::endl;
+		std::cin >> last_name;
+		new_contact.setLastName(last_name);
+		std::cout << "Nickname:" << std::endl;
+		std::cin >> nickname;
+		new_contact.setNickname(nickname);
+		std::cout << "Phone number:" << std::endl;
+		std::cin >> phone_number;
+		new_contact.setPhoneNumber(phone_number);
+		std::cout << "Darkest Secret:" << std::endl;
+		std::cin >> darkest_secret;
+		new_contact.setDarkestSecret(darkest_secret);
+		//i++;
+	//}
 }
