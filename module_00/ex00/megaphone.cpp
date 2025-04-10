d89a96eb58f8abe3ce9e9acfde817d776ec8712d
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2025-04-10 17:43:36 -0300

    improved

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -14,27 +14,22 @@
 
 void	to_upper(std::string str)
 {
-	int	i;
+	int		i;
 	char	upper;
 
-	for (i = 0; i < str.length(); i++)
+	for (i = 0; i < (int)str.length(); i++)
 	{
 		upper = (char)std::toupper(str[i]);
 		std::cout << upper;
-
 	}
 }
 
 int	main(int ac, char **av)
 {
-	int i = 1;
 	if (ac > 1)
 	{
-		while (av[i] != NULL)
-		{
+		for (int i = 1; av[i]; i++)
 			to_upper(av[i]);
-			i++;
-		}
 	}
 	else
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
