8513bfc2a878fdec9d3612dfcecdb7b7a70cbf3f
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2025-04-15 18:38:47 -0300

    updating output

--- module_01/ex02/main.cpp
+++ module_01/ex02/main.cpp
@@ -10,4 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <iomanip>
 
+int main(void) {
+
+    // Initializing variables
+    std::cout << "==================================================" << std::endl;
+    std::cout << "|            Initializing Variables:             |" << std::endl;
+    std::cout << "==================================================" << std::endl;
+    std::cout << std::endl;
+
+    std::string str = "HI THIS IS BRAIN";
+    std::string* stringPTR = &str;
+    std::string& stringREF = str;
+
+    std::cout << "============================================================================" << std::endl;
+    std::cout << "| " << std::left << std::setw(23) << "Identifier"
+              << "| " << std::left << std::setw(21) << "Memory Address"
+              << "| " << std::left << std::setw(25) << "Value Stored at Address" << "|" << std::endl;
+    std::cout << "============================================================================" << std::endl;
+
+    std::cout << "| " << std::left << std::setw(23) << "stringVariable"
+              << "| " << std::left << std::setw(21) << *stringPTR
+              << "| " << std::left << std::setw(25) << *stringPTR << "|" << std::endl;
+    std::cout << "============================================================================" << std::endl;
+    std::cout << "The memory address of the string variable: " << &str << std::endl;
+    std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+    std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "The value of the string variable: " << str << std::endl;
+    std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+     
+    return 0;
+}
