6fb1d889ac5d34e4280103c4853cb5b19facb0e6
lenovo_sabrina <sabrifer@sabrina>
2025-04-16 22:12:09 -0300

    output is nice

--- module_01/ex02/main.cpp
+++ module_01/ex02/main.cpp
@@ -16,35 +16,30 @@
 int main(void) {
 
     // Initializing variables
-    std::cout << "==================================================" << std::endl;
-    std::cout << "|            Initializing Variables:             |" << std::endl;
-    std::cout << "==================================================" << std::endl;
-    std::cout << std::endl;
-
     std::string str = "HI THIS IS BRAIN";
     std::string* stringPTR = &str;
     std::string& stringREF = str;
 
-    std::cout << "============================================================================" << std::endl;
+    std::cout << "=================================================================================" << std::endl;
     std::cout << "| " << std::left << std::setw(23) << "Identifier"
               << "| " << std::left << std::setw(21) << "Memory Address"
-              << "| " << std::left << std::setw(25) << "Value Stored at Address" << "|" << std::endl;
-    std::cout << "============================================================================" << std::endl;
+              << "| " << std::left << std::setw(30) << "Value Pointed to by Variable" << "|" << std::endl;
+    std::cout << "=================================================================================" << std::endl;
 
     std::cout << "| " << std::left << std::setw(23) << "stringVariable"
               << "| " << std::left << std::setw(21) << &str
-              << "| " << std::left << std::setw(25) << str << "|" << std::endl;
-    std::cout << "============================================================================" << std::endl;
+              << "| " << std::left << std::setw(30) << str << "|" << std::endl;
+    std::cout << "=================================================================================" << std::endl;
     
     std::cout << "| " << std::left << std::setw(23) << "stringPTR"
-              << "| " << std::left << std::setw(21) << stringPTR
-              << "| " << std::left << std::setw(25) << *stringPTR << "|" << std::endl;
-    std::cout << "============================================================================" << std::endl;
+              << "| " << std::left << std::setw(21) << &stringPTR
+              << "| " << std::left << std::setw(30) << stringPTR << "|" << std::endl;
+    std::cout << "=================================================================================" << std::endl;
     
     std::cout << "| " << std::left << std::setw(23) << "stringREF"
               << "| " << std::left << std::setw(21) << &stringREF
-              << "| " << std::left << std::setw(25) << stringREF << "|" << std::endl;
-    std::cout << "============================================================================" << std::endl;
+              << "| " << std::left << std::setw(30) << stringREF << "|" << std::endl;
+    std::cout << "=================================================================================" << std::endl;
      
     return 0;
 }
