8e4394e5d2a45b1a96001d8e4a659b391192fd8b
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2025-04-27 20:42:23 -0300

    working

--- module_03/ex00/ClapTrap.hpp
+++ module_03/ex00/ClapTrap.hpp
@@ -2,14 +2,24 @@
 #define CLAPTRAP_HPP
 
 #include <iostream>
+#include <iomanip>
+
+#define RESET       "\033[0m"
+#define RED         "\033[31m"
+#define GREEN       "\033[32m"
+#define YELLOW      "\033[33m"
+#define BLUE        "\033[34m"
+#define MAGENTA     "\033[35m"
+#define CYAN        "\033[36m"
+#define WHITE       "\033[37m"
 
 class   ClapTrap {
 
     private:
-        const std::string   name;
-        int                 hit_points;
-        int                 energy_points;
-        int                 attack_damage; 
+        std::string   name;
+        int                 hit_points; //represents life; reduces when attacked; can be increased.
+        int                 energy_points; // represents ability to act (attack and repair); cannot be increased.
+        int                 attack_damage; //
 
     public:
         ClapTrap(); // constructor
@@ -21,6 +31,8 @@ class   ClapTrap {
         void attack(const std::string& target);
         void takeDamage(unsigned int amount);
         void beRepaired(unsigned int amount);
+        void displayStatus() const;
 };
+
 
 #endif
