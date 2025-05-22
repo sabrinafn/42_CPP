e8ef1d2c4ccc6578830a6a502bcfcf9027e5215e
lenovo_sabrina <sabrifer@sabrina>
2025-05-22 19:44:41 -0300

    created class

---
+++ module_05/ex03/Intern.cpp
@@ -0,0 +1,18 @@
+#include "AForm.hpp"
+#include "Intern.hpp"
+
+// constructor
+Intern::Intern() {}
+
+// copy constructor
+Intern::Intern(const Intern &other) {}
+
+// copy assignment operator
+Intern& Intern::operator=(const Intern &other) {}
+
+// destructor
+Intern::~Intern() {}
+
+AForm* Intern::makeForm(const std::string &form_name, const std::string &target) {
+
+}
--- module_05/ex03/Intern.hpp
+++ module_05/ex03/Intern.hpp
@@ -1,8 +1,18 @@
 #ifndef INTERN_HPP
-#define INTERNET_HPP
+#define INTERN_HPP
+
+#include <iostream>
+#include "AForm.hpp"
 
 class Intern {
 
+    public:
+        Intern(); // constructor
+        Intern(const Intern &other); // copy constructor
+        Intern& operator=(const Intern &other); // copy assignment operator
+        ~Intern(); // destructor
+
+        AForm* makeForm(const std::string &form_name, const std::string &target);
 };
 
 #endif
