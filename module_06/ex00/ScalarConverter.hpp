ae1e2e8425a5963d232ed2173ceec0614a8b8f70
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2025-05-25 14:45:17 -0300

    ScalarConverter made not instantiable

---
+++ module_06/ex00/ScalarConverter.hpp
@@ -0,0 +1,22 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+
+class ScalarConverter {
+    
+    private:
+        ScalarConverter(); // constructor
+        ScalarConverter(const ScalarConverter &other); // copy constructor
+        ScalarConverter& operator=(const ScalarConverter &other); // copy assignment operator
+        ~ScalarConverter(); // destructor
+
+    public:
+        // accept a string representation of a scalar literal and
+        // print its corresponding values interpreted as char, int, float or double
+        static void convert(std::string &literal);
+
+
+};
+
+#endif
