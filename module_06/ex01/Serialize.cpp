d250944597990315e94f965fb9d9aadd17d8303f
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2025-05-28 20:59:13 -0300

    functions done?

--- module_06/ex01/Serialize.cpp
+++ module_06/ex01/Serialize.cpp
@@ -1,32 +1,26 @@
 #include "Serialize.hpp"
 
 // constructor
-Serializer::Serializer(void) {
-
-}
+Serializer::Serializer(void) {}
 
 // copy constructor
-Serializer::Serializer(const Serializer &other) {
-    
-}
+Serializer::Serializer(const Serializer &other) { (void)other; }
 
 // copy assignment operator
-Serializer& Serializer::operator=(const Serializer &other) {
-
-}
+Serializer& Serializer::operator=(const Serializer &other) { (void)other; return *this;}
 
 // destructor
-Serializer::~Serializer(void) {
+Serializer::~Serializer(void) {} 
 
-} 
-    
 
 // It takes a pointer and converts it to the unsigned integer type uintptr_t
 uintptr_t Serializer::serialize(Data* ptr) {
-
+    uintptr_t raw = reinterpret_cast<uintptr_t>(ptr);
+    return raw;
 }
 
 // It takes an unsigned integer parameter and converts it to a pointer to Data
 Data* Serializer::deserialize(uintptr_t raw) {
-    
+    Data* ptr = reinterpret_cast<Data*>(raw);
+    return ptr;
 }
