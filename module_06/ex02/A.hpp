59c5a8e72c90204381d51dc54f1436bc13459fea
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2025-05-29 14:53:46 -0300

    derived classes added

--- module_06/ex02/A.hpp
+++ module_06/ex02/A.hpp
@@ -1,7 +1,9 @@
 #ifndef A_HPP
 #define A_HPP
 
-class A {
+#include "Base.hpp"
+
+class A : public Base {
 
     public:
 
--- module_06/ex02/Base.hpp
+++ module_06/ex02/Base.hpp
@@ -2,9 +2,11 @@
 #define BASE_HPP
 
 class Base {
-
     public:
         virtual ~Base(void); // destructor
+        Base*   generate(void);
+        void    identify(Base* p);
+        void    identify(Base& p);
 };
 
 #endif
