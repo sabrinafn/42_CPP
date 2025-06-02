d13829344cf6b2528d1424d37c716dd0e2b6c293
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2025-06-02 19:59:56 -0300

    updating main

--- module_07/ex02/main.cpp
+++ module_07/ex02/main.cpp
@@ -1,33 +1,38 @@
 #include "Array.hpp"
 #include "Array.tpp"
 
+#define max_size 5
+
 int main(int, char**)
 {   
-    Array<int> myArrays2(10);
+    Array<int> myArrays2(max_size);
     int num = 123;
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < max_size; i++) {
         myArrays2[i] = num++;
     }
     
     std::cout << "Array<int>myArrays2 created with values: " << std::endl;
-    for (int i = 0; i < 10; i++) {
-        std::cout << "[" << i << "]: " << myArrays2[i] << std::endl;
+    for (int i = 0; i < max_size; i++) {
+        std::cout << "[" << i << "]: " << myArrays2[i] << " ";
     }
-
+    std::cout << "\n" << std::endl;
 
     Array<int> myArrayDeepCopy = myArrays2;
     Array<int> test(myArrayDeepCopy);
     
-    std::cout << "Array<int>myArrayDeepCopy created with values: " << std::endl;
-    for (int i = 0; i < 10; i++) {
-        std::cout << "[" << i << "]: " << myArrayDeepCopy[i] << std::endl;
+    std::cout << "Array<int>myArrayDeepCopy (=operator) created with values: " << std::endl;
+    for (int i = 0; i < max_size; i++) {
+        std::cout << "[" << i << "]: " << myArrayDeepCopy[i] << " ";
     }
+    std::cout << "\n" << std::endl;
 
-    std::cout << "Array<int>test created with values: " << std::endl;
-    for (int i = 0; i < 10; i++) {
-        std::cout << "[" << i << "]: " << test[i] << std::endl;
+    std::cout << "Array<int>test (copy constructor) created with values: " << std::endl;
+    for (int i = 0; i < max_size; i++) {
+        std::cout << "[" << i << "]: " << test[i] << " ";
     }
+    std::cout << "\n" << std::endl;
 
+    std::cout << "Trying to access position -2"
     try
     {
         myArrays2[-2] = 0;
@@ -38,14 +43,14 @@ int main(int, char**)
     }
     try
     {
-        myArrays2[10] = 0;
+        myArrays2[max_size] = 0;
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
     }
 
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < max_size; i++)
     {
         myArrays2[i] = num++;
     }
