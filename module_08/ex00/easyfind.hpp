5cddf629c255919d26516edf3ec61dc2fdefceb4
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2025-06-03 19:25:00 -0300

    testing function

--- module_08/ex00/easyfind.hpp
+++ module_08/ex00/easyfind.hpp
@@ -1,10 +1,19 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <algorithm>
+
 template <typename T>
 void easyfind(T& container, int i) {
 
+    std::vector<int>::iterator it;
 
+    it = find(container.begin(), container.end(), i);
+
+    if (it != container.end())
+        std::cout << "element found in T container: " << *it << std::endl;
+    else
+        std::cout << "element not found in T container." << std::endl;
 };
 
 #endif
