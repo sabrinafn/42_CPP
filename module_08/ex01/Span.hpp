45031b45825093a8d3266d16aec67540a0450a60
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2025-06-05 17:34:59 -0300

    created class

--- module_08/ex01/Span.hpp
+++ module_08/ex01/Span.hpp
@@ -1,10 +1,31 @@
-#include SPAN_HPP
+#ifndef SPAN_HPP
 #define SPAN_HPP
+
+#include <vector>
+#include "iostream"
 
 class Span {
 
     private:
-        
+        unsigned int max_size;
+        std::vector<int> vec;
+
+    public:
+        /* CONSTRUCTORS */
+        Span(void); // constructor
+        Span(unsigned int N); // constructor unsigned int
+        Span(const Span& other); // copy constructor
+
+        /* DESTRUCTOR */
+        ~Span(void);
+
+        /* OPERATORS */
+        Span& operator=(const Span& other); // copy assignment operator
+
+        void addNumber(int N); // add number to Span
+        //void addRange();
+        void shortestSpan(void); // shortest distance between all numbers
+        void longestSpan(void); // longest distance between all numbers
 };
 
 #endif
