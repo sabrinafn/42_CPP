2a68c5b804ef2fc44864d09f2242a3ff87a1a7e7
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2025-06-09 17:27:01 -0300

    working, still testing

--- module_08/ex02/MutantStack.hpp
+++ module_08/ex02/MutantStack.hpp
@@ -4,9 +4,37 @@
 #include <iostream>
 #include <stack>
 
+// template is for this: when a container is created, I need to
+// add the type of variable it will be
+// like std::vector<int>
+//      std::deque<char>
+//      std::stack<string>
+
 template<typename T>
 class MutantStack : public std::stack<T> {
 
+    public: 
+        /* CONSTRUCTORS */
+        MutantStack(void) {} // constructor
+        MutantStack(const MutantStack& other) {
+            *this = other;
+        } // copy constructor
+
+        /* DESTRUCTOR */
+        ~MutantStack(void) {} // destructor
+
+        /* OPERATORS */
+        // copy assignment operator
+        MutantStack& operator=(const MutantStack& other) {
+            (void)other;
+            return *this;
+        }
+
+        typedef typename std::stack<T>::container_type::iterator iterator;
+
+        // c = container interno de std::stack
+        iterator begin() { return this->c.begin(); }
+        iterator end() { return this->c.end(); }
 };
 
 #endif
--- module_08/ex02/main.cpp
+++ module_08/ex02/main.cpp
@@ -3,25 +3,38 @@
 int main()
 {
     MutantStack<int> mstack;
+    std::cout << "adding 5 to the stack" << std::endl;
     mstack.push(5);
+    std::cout << "adding 17 to the stack\n" << std::endl;
     mstack.push(17);
+    std::cout << "printing value at the top: ";
     std::cout << mstack.top() << std::endl;
+    std::cout << "removing value at the top" << std::endl;
     mstack.pop();
+    std::cout << "printing stack size: ";
     std::cout << mstack.size() << std::endl;
+
+    std::cout << "\nadding 3 to the stack" << std::endl;
     mstack.push(3);
+    std::cout << "adding 5 to the stack" << std::endl;
     mstack.push(5);
+    std::cout << "adding 737 to the stack" << std::endl;
     mstack.push(737);
     //[...]
+    std::cout << "adding 0 to the stack" << std::endl;
     mstack.push(0);
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
     ++it;
     --it;
+    std::cout << "\nprinting whole stack: ";
+    std::cout << "{ ";
     while (it != ite)
     {
-        std::cout << *it << std::endl;
+        std::cout << *it << "  ";
         ++it;
     }
+    std::cout << "}";
     std::stack<int> s(mstack);
     return 0;
 }
