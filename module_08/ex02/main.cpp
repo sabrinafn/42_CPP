cf2739cb7edacca6b27ac126a125f6990054472d
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2025-06-09 17:36:39 -0300

    deleted useless files; updated main

--- module_08/ex02/main.cpp
+++ module_08/ex02/main.cpp
@@ -7,10 +7,13 @@ int main()
     mstack.push(5);
     std::cout << "adding 17 to the stack\n" << std::endl;
     mstack.push(17);
+
     std::cout << "printing value at the top: ";
     std::cout << mstack.top() << std::endl;
+
     std::cout << "removing value at the top" << std::endl;
     mstack.pop();
+
     std::cout << "printing stack size: ";
     std::cout << mstack.size() << std::endl;
 
@@ -20,13 +23,20 @@ int main()
     mstack.push(5);
     std::cout << "adding 737 to the stack" << std::endl;
     mstack.push(737);
-    //[...]
+    
     std::cout << "adding 0 to the stack" << std::endl;
     mstack.push(0);
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
+
+    std::cout << "iterator stack.begin(): " << *it << std::endl;
+    std::cout << "iterator stack.end(): " << *ite << std::endl;
+
     ++it;
+    std::cout << "after incrementing iterator stack.begin(): " << *it << std::endl;
     --it;
+    std::cout << "after decrementing iterator stack.begin(): " << *it << std::endl;
+
     std::cout << "\nprinting whole stack: ";
     std::cout << "{ ";
     while (it != ite)
