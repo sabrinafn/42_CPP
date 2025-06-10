ec130f80383579396bc3783df4232fbaeeb6c241
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2025-06-10 18:19:12 -0300

    implemented class

---
+++ module_09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,27 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <map>
+
+class MyMap {
+
+    private:
+        std::map<std::string, double> container;
+    
+    public:
+        
+        /* CONSTRUCTORS */
+        MyMap(void);
+        MyMap(const MyMap &other);
+
+        /* DESTRUCTORS */
+        ~MyMap(void);
+
+        /* OPERATORS */
+        MyMap& operator=(const MyMap &other);
+
+        
+};
+
+#endif
---
+++ module_09/ex00/main.cpp
@@ -0,0 +1,9 @@
+#include "BitcoinExchange.hpp"
+
+int main(int ac, char **av) {
+
+    if (ac != 2)
+        std::cerr << "Error! Wrong number of arguments." << std::endl;
+
+    return 0;
+}
