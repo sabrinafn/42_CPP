e9639d58af222c770c141ed82fb13fa4bb682d7c
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2025-06-10 17:27:24 -0300

    cleaning .hpp

--- module_08/ex02/MutantStack.hpp
+++ module_08/ex02/MutantStack.hpp
@@ -4,40 +4,31 @@
 #include <iostream>
 #include <stack>
 
-// template is for this: when a container is created, I need to
-// add the type of variable it will be
-//      std::vector<int>
-//      std::deque<char>
-//      std::stack<string>
-
 template<typename T>
 class MutantStack : public std::stack<T> {
 
     public: 
         /* CONSTRUCTORS */
-        MutantStack(void) {} // constructor
-        MutantStack(const MutantStack& other) { *this = other; } // copy constructor
+        MutantStack(void) {}
+        MutantStack(const MutantStack& other) { *this = other; }
 
         /* DESTRUCTOR */
-        ~MutantStack(void) {} // destructor
+        ~MutantStack(void) {}
 
         /* OPERATORS */
-        // copy assignment operator
+        // copy assignment
         MutantStack& operator=(const MutantStack& other) {
             if (this != &other)
-                std::stack<T>::operator=(other); // using base class assignment operator
-            return *this;
-        }
+                std::stack<T>::operator=(other); // using base class
+            return *this; }
 
-        // declaring iterator 
+        /* ITERATOR */  
+
         // std::stack<T>::container_type == std::deque<T>
         typedef typename std::stack<T>::container_type::iterator iterator;
         typedef typename std::stack<T>::container_type::const_iterator const_iterator;
-        // this is almost the same as this:
-        //typedef typename std::deque<T>::iterator iterator;
 
-
-        // c = object holding elements inside of std::stack
+        // c = container inside of std::stack
         iterator begin() {
             return this->c.begin();}
         iterator end() {
@@ -53,7 +44,7 @@ class MutantStack : public std::stack<T> {
 template <typename T>
 std::ostream& operator<<(std::ostream& os, const MutantStack<T> &other) {
 
-    // compiler needs typename to identify that iterator is a type
+    // compiler needs typename to identify that the iterator is a type of var
     typename MutantStack<T>::const_iterator it = other.begin(); // able to access begin()
     typename MutantStack<T>::const_iterator ite = other.end(); // and end()
 
