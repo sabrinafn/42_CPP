0307f8f39811755a2bed76f05bfd6d4384a5d6cf
lenovo_sabrina <sabrinaferreiran@gmail.com>
2025-06-19 11:22:05 -0300

    finished parser

--- module_09/ex01/RPN.cpp
+++ module_09/ex01/RPN.cpp
@@ -30,18 +30,29 @@ RPN& RPN::operator=(const RPN &other) {
 bool RPN::parseExpression(const std::string &expression) {
     // numbers 0 to 9 and operators "+ - / *"
     // validate spaces between each digit
+    // number of operators = number of operands - 1
+    int digit_count = 0;
+    int operator_count = 0;
     for (size_t i = 0; i < expression.length(); i++) {
-        if (!isdigit(expression[i]) && !isOperator(expression[i]))
-            throw std::invalid_argument("Error: invalid characters in expression");
-    }
-    return false;
-}
-
-bool RPN::isOperator(const char c) {
-
-    for (size_t i = 0; i < OPERATORS.length(); i++) {
-        if (OPERATORS[i] != c)
+        char c = expression[i];
+        if (i % 2 != 0) {
+            if (c != ' ') {
+                std::cerr << "Error: invalid characters in expression" << std::endl;
+                return false;
+            }
+        }
+        else if (isdigit(c))
+            digit_count++;
+        else if (OPERATORS.find(c) != std::string::npos)
+            operator_count++;
+        else {
+            std::cerr << "Error: invalid characters in expression" << std::endl;
             return false;
+        }
+    }
+    if (operator_count != digit_count - 1) {
+        std::cerr << "Error: invalid characters in expression" << std::endl;
+        return false;
     }
     return true;
 }
